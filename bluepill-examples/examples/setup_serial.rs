abf19b6e5d227dd0d046461056bfc571684afb17
Mendelt Siebenga <msiebenga@gmail.com>
2020-06-09 21:31:27 +0200

    fix serial setup

--- bluepill-examples/examples/setup_serial.rs
+++ bluepill-examples/examples/setup_serial.rs
@@ -2,17 +2,18 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::pac;
-use stm32f1xx_hal::prelude::*;
-use stm32f1xx_hal::time::U32Ext;
-use stm32f1xx_hal::serial::Serial;
+use stm32f1xx_hal::{
+    pac,
+    prelude::*,
+    serial::{Config, Serial},
+};
 
 #[allow(unused_imports)]
 use panic_semihosting;
 
 #[entry]
 fn main() -> ! {
-    let cp = cortex_m::Peripherals::take().unwrap();
+    // let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
     // Configure the clock
@@ -20,17 +21,27 @@ fn main() -> ! {
     let mut flash = dp.FLASH.constrain();
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
+    // Prepare the alternate function I/O registers
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
     // Configure pins for serial rx/tx
-    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-    let tx = gpiob.pb3.into_alternate_open_drain(&mut gpiob.crl);
-    let rx = gpiob.pb2.into_alternate_open_drain(&mut gpiob.crl);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+    let rx = gpioa.pa3;
 
     // Configure serial
-    let uart = Serial::usart2(dp.USART2, (tx, rx), 115_200.bps(), clocks, &mut rcc.apb2);
+    let usart = Serial::usart2(
+        dp.USART2,
+        (tx, rx),
+        &mut afio.mapr,
+        Config::default().baudrate(115_200.bps()),
+        clocks,
+        &mut rcc.apb1,
+    );
 
     // Configure Midi
     // <TODO>
     loop {
-       // Wait for note on and print to semihosting output
+        // Wait for note on and print to semihosting output
     }
 }
