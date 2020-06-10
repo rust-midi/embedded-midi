401b7ef3f6a8dc06e4d58c9c1bd1598443ef10e5
Mendelt Siebenga <msiebenga@gmail.com>
2020-06-10 21:59:37 +0200

    library parse note on/off naive, hardcoded channel 0

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,42 @@
 //!
 #![no_std]
 use embedded_hal::serial;
+use nb::block;
 
 mod error;
 mod midi;
+
+pub use midi::MidiEvent;
 
 pub struct MidiIn<RX> {
     rx: RX,
 }
 
-impl<RX> MidiIn<RX> where RX: serial::Read<u8> {}
+impl<RX, E> MidiIn<RX>
+where
+    RX: serial::Read<u8, Error = E>,
+{
+    pub fn new(rx: RX) -> Self {
+        MidiIn { rx }
+    }
+
+    // naive implementation, block until we've received a midi event we understand
+    pub fn read(&mut self) -> Option<MidiEvent> {
+        match block!(self.rx.read()) {
+            Ok(90) => Some(MidiEvent::note_on(
+                midi::Channel::from(0),
+                midi::Note::from(block!(self.rx.read()).unwrap_or(0)),
+                midi::Velocity::from(block!(self.rx.read()).unwrap_or(0)),
+            )),
+            Ok(80) => Some(MidiEvent::note_off(
+                midi::Channel::from(0),
+                midi::Note::from(block!(self.rx.read()).unwrap_or(0)),
+                midi::Velocity::from(block!(self.rx.read()).unwrap_or(0)),
+            )),
+            _ => None,
+        }
+    }
+}
 
 pub struct MidiOut<TX> {
     tx: TX,
