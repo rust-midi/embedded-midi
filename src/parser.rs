15e6a3dd8c1d6104add05eb6ad4145b732b89325
Mendelt Siebenga <msiebenga@gmail.com>
2020-06-14 22:12:38 +0200

    refactor midi parsing to use status vs data bytes

--- src/parser.rs
+++ src/parser.rs
@@ -13,6 +13,10 @@ enum MidiParserState {
     NoteOffNoteRecvd { channel: u8, note: u8 },
 }
 
+fn is_status_byte(byte: u8) -> bool {
+    byte & 0x80 == 0x80
+}
+
 impl MidiParser {
     /// Initialize midiparser state
     pub fn new() -> Self {
@@ -25,52 +29,65 @@ impl MidiParser {
     /// completed it is returned, otherwise this method updates the internal midiparser state and
     /// and returns none.
     pub fn parse_byte(&mut self, byte: u8) -> Option<MidiEvent> {
-        match self.state {
-            MidiParserState::Idle => {
-                // expect the start of a new message
-                let message = byte & 0xf0u8;
-                let channel = byte & 0x0fu8;
+        if is_status_byte(byte) {
+            let message = byte & 0xf0u8;
+            let channel = byte & 0x0fu8;
 
-                match message {
-                    0x80 => {
-                        self.state = MidiParserState::NoteOffRecvd { channel };
-                        None
-                    }
-                    0x90 => {
-                        self.state = MidiParserState::NoteOnRecvd { channel };
-                        None
-                    }
-                    _ => None,
+            match message {
+                0x80 => {
+                    self.state = MidiParserState::NoteOffRecvd { channel };
+                    None
                 }
+                0x90 => {
+                    self.state = MidiParserState::NoteOnRecvd { channel };
+                    None
+                }
+                _ => None,
             }
+        } else {
+            match self.state {
+                MidiParserState::NoteOnRecvd { channel } => {
+                    self.state = MidiParserState::NoteOnNoteRecvd {
+                        channel,
+                        note: byte,
+                    };
+                    None
+                }
+                MidiParserState::NoteOnNoteRecvd { channel, note } => {
+                    self.state = MidiParserState::Idle;
+                    Some(MidiEvent::note_on(channel.into(), note.into(), byte.into()))
+                }
 
-            MidiParserState::NoteOnRecvd { channel } => {
-                self.state = MidiParserState::NoteOnNoteRecvd {
-                    channel,
-                    note: byte,
-                };
-                None
-            }
-            MidiParserState::NoteOnNoteRecvd { channel, note } => {
-                self.state = MidiParserState::Idle;
-                Some(MidiEvent::note_on(channel.into(), note.into(), byte.into()))
-            }
-
-            MidiParserState::NoteOffRecvd { channel } => {
-                self.state = MidiParserState::NoteOffNoteRecvd {
-                    channel,
-                    note: byte,
-                };
-                None
-            }
-            MidiParserState::NoteOffNoteRecvd { channel, note } => {
-                self.state = MidiParserState::Idle;
-                Some(MidiEvent::note_off(
-                    channel.into(),
-                    note.into(),
-                    byte.into(),
-                ))
+                MidiParserState::NoteOffRecvd { channel } => {
+                    self.state = MidiParserState::NoteOffNoteRecvd {
+                        channel,
+                        note: byte,
+                    };
+                    None
+                }
+                MidiParserState::NoteOffNoteRecvd { channel, note } => {
+                    self.state = MidiParserState::Idle;
+                    Some(MidiEvent::note_off(
+                        channel.into(),
+                        note.into(),
+                        byte.into(),
+                    ))
+                }
+                _ => None,
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_status_byte_should_test_msb() {
+        assert!(is_status_byte(0x80u8));
+        assert!(is_status_byte(0x94u8));
+        assert!(!is_status_byte(0x00u8));
+        assert!(!is_status_byte(0x78u8));
     }
 }
