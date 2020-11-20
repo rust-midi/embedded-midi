06d92a6cebb3f332bc630cd62ea9ffaca682491d
David Stalnaker <david.stalnaker@gmail.com>
2020-11-20 09:15:04 -0800

    Take reference to message in write instead of taking ownership.

--- src/lib.rs
+++ src/lib.rs
@@ -50,82 +50,82 @@ where
         MidiOut { tx }
     }
 
-    pub fn write(&mut self, event: MidiMessage) -> Result<(), E> {
+    pub fn write(&mut self, event: &MidiMessage) -> Result<(), E> {
         match event {
-            MidiMessage::NoteOn(channel, note, velocity) => {
+            &MidiMessage::NoteOn(channel, note, velocity) => {
                 let channelnum: u8 = channel.into();
                 block!(self.tx.write(0x90 + channelnum))?;
                 block!(self.tx.write(note.into()))?;
                 block!(self.tx.write(velocity.into()))?;
             }
-            MidiMessage::NoteOff(channel, note, velocity) => {
+            &MidiMessage::NoteOff(channel, note, velocity) => {
                 let channelnum: u8 = channel.into();
                 block!(self.tx.write(0x80 + channelnum))?;
                 block!(self.tx.write(note.into()))?;
                 block!(self.tx.write(velocity.into()))?;
             }
-            MidiMessage::KeyPressure(channel, note, value) => {
+            &MidiMessage::KeyPressure(channel, note, value) => {
                 let channelnum: u8 = channel.into();
                 block!(self.tx.write(0xA0 + channelnum))?;
                 block!(self.tx.write(note.into()))?;
                 block!(self.tx.write(value.into()))?;
             }
-            MidiMessage::ControlChange(channel, control, value) => {
+            &MidiMessage::ControlChange(channel, control, value) => {
                 let channelnum: u8 = channel.into();
                 block!(self.tx.write(0xB0 + channelnum))?;
                 block!(self.tx.write(control.into()))?;
                 block!(self.tx.write(value.into()))?;
             }
-            MidiMessage::ProgramChange(channel, program) => {
+            &MidiMessage::ProgramChange(channel, program) => {
                 let channelnum: u8 = channel.into();
                 block!(self.tx.write(0xC0 + channelnum))?;
                 block!(self.tx.write(program.into()))?;
             }
-            MidiMessage::ChannelPressure(channel, value) => {
+            &MidiMessage::ChannelPressure(channel, value) => {
                 let channelnum: u8 = channel.into();
                 block!(self.tx.write(0xD0 + channelnum))?;
                 block!(self.tx.write(value.into()))?;
             }
-            MidiMessage::PitchBendChange(channel, value) => {
+            &MidiMessage::PitchBendChange(channel, value) => {
                 let channelnum: u8 = channel.into();
                 let (first_byte, second_byte) = value.into();
                 block!(self.tx.write(0xE0 + channelnum))?;
                 block!(self.tx.write(first_byte))?;
                 block!(self.tx.write(second_byte))?;
             }
-            MidiMessage::QuarterFrame(value) => {
+            &MidiMessage::QuarterFrame(value) => {
                 block!(self.tx.write(0xF1))?;
                 block!(self.tx.write(value.into()))?;
             }
-            MidiMessage::SongPositionPointer(value) => {
+            &MidiMessage::SongPositionPointer(value) => {
                 let (first_byte, second_byte) = value.into();
                 block!(self.tx.write(0xF2))?;
                 block!(self.tx.write(first_byte))?;
                 block!(self.tx.write(second_byte))?;
             }
-            MidiMessage::SongSelect(value) => {
+            &MidiMessage::SongSelect(value) => {
                 block!(self.tx.write(0xF3))?;
                 block!(self.tx.write(value.into()))?;
             }
-            MidiMessage::TuneRequest => {
+            &MidiMessage::TuneRequest => {
                 block!(self.tx.write(0xF6))?;
             }
-            MidiMessage::TimingClock => {
+            &MidiMessage::TimingClock => {
                 block!(self.tx.write(0xF8))?;
             }
-            MidiMessage::Start => {
+            &MidiMessage::Start => {
                 block!(self.tx.write(0xFA))?;
             }
-            MidiMessage::Continue => {
+            &MidiMessage::Continue => {
                 block!(self.tx.write(0xFB))?;
             }
-            MidiMessage::Stop => {
+            &MidiMessage::Stop => {
                 block!(self.tx.write(0xFC))?;
             }
-            MidiMessage::ActiveSensing => {
+            &MidiMessage::ActiveSensing => {
                 block!(self.tx.write(0xFE))?;
             }
-            MidiMessage::Reset => {
+            &MidiMessage::Reset => {
                 block!(self.tx.write(0xFF))?;
             }
         }
