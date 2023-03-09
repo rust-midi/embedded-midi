980ec597719bc0ff31fb16eb4112721d1e61d0dd
Mendelt Siebenga <mendelt@mendelt.nl>
2023-03-09 22:44:03 +0100

    use the stateful renderer from midi-convert
    bump msrv to 1.62.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,23 @@
 //! *Midi driver on top of embedded hal serial communications*
-//!
+
 #![no_std]
 #![warn(missing_debug_implementations)]
-use {
-    core::fmt::Debug,
-    embedded_hal::serial,
-    midi_convert::{midi_types::MidiMessage, MidiByteStreamParser, MidiRenderSlice},
-    nb::block,
+use core::fmt::Debug;
+use embedded_hal::serial;
+use midi_convert::midi_types::MidiMessage;
+
+use midi_convert::{
+    parse::MidiParser,
+    render::{MidiRenderer, MidiTransport},
 };
+use nb::block;
 
 pub use midi_convert::midi_types;
 
 #[derive(Debug)]
 pub struct MidiIn<RX> {
     rx: RX,
-    parser: MidiByteStreamParser,
+    parser: MidiParser,
 }
 
 impl<RX, E> MidiIn<RX>
@@ -25,7 +28,7 @@ where
     pub fn new(rx: RX) -> Self {
         MidiIn {
             rx,
-            parser: MidiByteStreamParser::new(),
+            parser: MidiParser::new(),
         }
     }
 
@@ -40,8 +43,25 @@ where
 }
 
 #[derive(Debug)]
+struct SerialTransport<TX>(TX);
+
+impl<TX, E> MidiTransport for SerialTransport<TX>
+where
+    TX: serial::Write<u8, Error = E>,
+    E: Debug,
+{
+    type Error = E;
+
+    fn write(&mut self, bytes: &[u8]) -> Result<(), Self::Error> {
+        bytes
+            .iter()
+            .try_for_each(|value| block!(self.0.write(*value)))
+    }
+}
+
+#[derive(Debug)]
 pub struct MidiOut<TX> {
-    tx: TX,
+    renderer: MidiRenderer<SerialTransport<TX>, true>,
 }
 
 impl<TX, E> MidiOut<TX>
@@ -50,21 +70,17 @@ where
     E: Debug,
 {
     pub fn new(tx: TX) -> Self {
-        MidiOut { tx }
+        MidiOut {
+            renderer: MidiRenderer::new(SerialTransport(tx)),
+        }
     }
 
     pub fn release(self) -> TX {
-        self.tx
+        self.renderer.release().0
     }
 
     pub fn write(&mut self, message: &MidiMessage) -> Result<(), E> {
-        let mut buf = [0u8; 3];
-        let len = message.render_slice(buf.as_mut());
-        for b in buf.iter().take(len) {
-            block!(self.tx.write(*b))?;
-        }
-
-        Ok(())
+        self.renderer.render(message)
     }
 }
 
@@ -90,16 +106,15 @@ mod tests {
     }
 
     #[test]
-    fn note_on_should_write_successfully() {
+    fn should_write_midi_message() {
         verify_writes(
             &[MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into())],
             &[0x92, 0x76, 0x34],
         );
     }
 
-    /*
     #[test]
-    fn note_on_second_note_should_skip_status() {
+    fn should_use_running_status() {
         verify_writes(
             &[
                 MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into()),
@@ -107,123 +122,5 @@ mod tests {
             ],
             &[0x92, 0x76, 0x34, 0x33, 0x65],
         );
-    }
-    */
-
-    #[test]
-    fn note_on_second_note_different_channel_should_not_skip_status() {
-        verify_writes(
-            &[
-                MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into()),
-                MidiMessage::NoteOn(0x03.into(), 0x33.into(), 0x65.into()),
-            ],
-            &[0x92, 0x76, 0x34, 0x93, 0x33, 0x65],
-        );
-    }
-    #[test]
-    fn note_on_note_off_should_not_skip_status() {
-        verify_writes(
-            &[
-                MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into()),
-                MidiMessage::NoteOff(0x02.into(), 0x33.into(), 0x65.into()),
-            ],
-            &[0x92, 0x76, 0x34, 0x82, 0x33, 0x65],
-        );
-    }
-    #[test]
-    fn note_off_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::NoteOff(0x02.into(), 0x76.into(), 0x34.into())],
-            &[0x82, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn key_pressure_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::KeyPressure(
-                0x02.into(),
-                0x76.into(),
-                0x34.into(),
-            )],
-            &[0xA2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn control_change_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::ControlChange(
-                0x02.into(),
-                0x76.into(),
-                0x34.into(),
-            )],
-            &[0xB2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn program_change_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::ProgramChange(0x02.into(), 0x76.into())],
-            &[0xC2, 0x76],
-        );
-    }
-    #[test]
-    fn channel_pressure_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::ChannelPressure(0x02.into(), 0x76.into())],
-            &[0xD2, 0x76],
-        );
-    }
-    #[test]
-    fn pitch_bend_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::PitchBendChange(
-                0x02.into(),
-                (0x76, 0x34).into(),
-            )],
-            &[0xE2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn quarter_frame_should_write_successfully() {
-        verify_writes(&[MidiMessage::QuarterFrame(0x76.into())], &[0xF1, 0x76]);
-    }
-    #[test]
-    fn song_position_pointer_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::SongPositionPointer((0x76, 0x34).into())],
-            &[0xF2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn song_select_should_write_successfully() {
-        verify_writes(&[MidiMessage::SongSelect(0x76.into())], &[0xF3, 0x76]);
-    }
-    #[test]
-    fn tune_request_should_write_successfully() {
-        verify_writes(&[MidiMessage::TuneRequest], &[0xF6]);
-    }
-    #[test]
-    fn timing_clock_should_write_successfully() {
-        verify_writes(&[MidiMessage::TimingClock], &[0xF8]);
-    }
-    #[test]
-    fn start_should_write_successfully() {
-        verify_writes(&[MidiMessage::Start], &[0xFA]);
-    }
-    #[test]
-    fn continue_should_write_successfully() {
-        verify_writes(&[MidiMessage::Continue], &[0xFB]);
-    }
-    #[test]
-    fn stop_should_write_successfully() {
-        verify_writes(&[MidiMessage::Stop], &[0xFC]);
-    }
-    #[test]
-    fn active_sensing_should_write_successfully() {
-        verify_writes(&[MidiMessage::ActiveSensing], &[0xFE]);
-    }
-    #[test]
-    fn reset_should_write_successfully() {
-        verify_writes(&[MidiMessage::Reset], &[0xFF]);
     }
 }
