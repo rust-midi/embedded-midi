2154cb194f0c5d39f18fc4fecdfc46c4feb8691f
Mendelt Siebenga <mendelt@mendelt.nl>
2023-03-19 22:04:38 +0100

    move rendering tests to midi-convert repository

--- src/lib.rs
+++ src/lib.rs
@@ -106,7 +106,7 @@ mod tests {
     }
 
     #[test]
-    fn note_on_should_write_successfully() {
+    fn should_write_midi_message() {
         verify_writes(
             &[MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into())],
             &[0x92, 0x76, 0x34],
@@ -114,7 +114,7 @@ mod tests {
     }
 
     #[test]
-    fn note_on_second_note_should_skip_status() {
+    fn should_use_running_status() {
         verify_writes(
             &[
                 MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into()),
@@ -122,122 +122,5 @@ mod tests {
             ],
             &[0x92, 0x76, 0x34, 0x33, 0x65],
         );
-    }
-
-    #[test]
-    fn note_on_second_note_different_channel_should_not_skip_status() {
-        verify_writes(
-            &[
-                MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into()),
-                MidiMessage::NoteOn(0x03.into(), 0x33.into(), 0x65.into()),
-            ],
-            &[0x92, 0x76, 0x34, 0x93, 0x33, 0x65],
-        );
-    }
-    #[test]
-    fn note_on_note_off_should_not_skip_status() {
-        verify_writes(
-            &[
-                MidiMessage::NoteOn(0x02.into(), 0x76.into(), 0x34.into()),
-                MidiMessage::NoteOff(0x02.into(), 0x33.into(), 0x65.into()),
-            ],
-            &[0x92, 0x76, 0x34, 0x82, 0x33, 0x65],
-        );
-    }
-    #[test]
-    fn note_off_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::NoteOff(0x02.into(), 0x76.into(), 0x34.into())],
-            &[0x82, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn key_pressure_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::KeyPressure(
-                0x02.into(),
-                0x76.into(),
-                0x34.into(),
-            )],
-            &[0xA2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn control_change_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::ControlChange(
-                0x02.into(),
-                0x76.into(),
-                0x34.into(),
-            )],
-            &[0xB2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn program_change_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::ProgramChange(0x02.into(), 0x76.into())],
-            &[0xC2, 0x76],
-        );
-    }
-    #[test]
-    fn channel_pressure_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::ChannelPressure(0x02.into(), 0x76.into())],
-            &[0xD2, 0x76],
-        );
-    }
-    #[test]
-    fn pitch_bend_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::PitchBendChange(
-                0x02.into(),
-                (0x76, 0x34).into(),
-            )],
-            &[0xE2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn quarter_frame_should_write_successfully() {
-        verify_writes(&[MidiMessage::QuarterFrame(0x76.into())], &[0xF1, 0x76]);
-    }
-    #[test]
-    fn song_position_pointer_should_write_successfully() {
-        verify_writes(
-            &[MidiMessage::SongPositionPointer((0x76, 0x34).into())],
-            &[0xF2, 0x76, 0x34],
-        );
-    }
-    #[test]
-    fn song_select_should_write_successfully() {
-        verify_writes(&[MidiMessage::SongSelect(0x76.into())], &[0xF3, 0x76]);
-    }
-    #[test]
-    fn tune_request_should_write_successfully() {
-        verify_writes(&[MidiMessage::TuneRequest], &[0xF6]);
-    }
-    #[test]
-    fn timing_clock_should_write_successfully() {
-        verify_writes(&[MidiMessage::TimingClock], &[0xF8]);
-    }
-    #[test]
-    fn start_should_write_successfully() {
-        verify_writes(&[MidiMessage::Start], &[0xFA]);
-    }
-    #[test]
-    fn continue_should_write_successfully() {
-        verify_writes(&[MidiMessage::Continue], &[0xFB]);
-    }
-    #[test]
-    fn stop_should_write_successfully() {
-        verify_writes(&[MidiMessage::Stop], &[0xFC]);
-    }
-    #[test]
-    fn active_sensing_should_write_successfully() {
-        verify_writes(&[MidiMessage::ActiveSensing], &[0xFE]);
-    }
-    #[test]
-    fn reset_should_write_successfully() {
-        verify_writes(&[MidiMessage::Reset], &[0xFF]);
     }
 }
